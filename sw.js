03fc627c1925b64acaeb479366fab86b3df98d3e
vanessamora2060-cmyk <vanessamora2060@gmail.com>
2026-07-16 21:24:19 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'naju-v1';
+const ASSETS = [
+  'index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
